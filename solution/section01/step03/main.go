fc12565eee5174fb613a74ba640109706465883b
tenntenn <uedatakuya@gmail.com>
2021-02-09 04:10:54 +0900

    Add step03

---
+++ solution/section01/step03/main.go
@@ -0,0 +1,23 @@
+// STEP03: ランダムな数字の表示
+
+package main
+
+import (
+	// TODO: fmtパッケージをインポートする
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	// 乱数の種を設定する
+	// 現在時刻をUNIX時間にしたものを種とする
+	rand.Seed(time.Now().Unix())
+
+	// TODO: 0から9までの間で乱数を作り変数numに代入する
+	num := rand.Intn(10)
+
+	// 変数numを表示する
+	fmt.Println(num)
+}
