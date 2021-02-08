12f367111fb2ce659176e23fa0695084d3577994
tenntenn <uedatakuya@gmail.com>
2021-02-09 06:42:46 +0900

    Add sec02

---
+++ solution/section02/step01/main.go
@@ -0,0 +1,43 @@
+// STEP08: ガチャを選べるようにしよう
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	// 乱数の種を設定する
+	// 現在時刻をUNIX時間にしたものを種とする
+	rand.Seed(time.Now().Unix())
+
+	// TODO: 長さ11の文字列型の配列を変数resultとして定義する
+	var result [11]string
+
+	// TODO: 継続条件をiがresultの要素数より小さい場合にする
+	for i := 0; i < len(result); i++ {
+
+		// 0から99までの間で乱数を生成する
+		num := rand.Intn(99)
+
+		// 変数numが0〜79のときは"ノーマル"、
+		// 80〜94のときは"R"、95〜98のときは"SR"、
+		// それ以外のときは"XR"と表示する
+		switch {
+		case num < 80:
+			result[i] = "ノーマル"
+		case num < 95:
+			// resultのi番目に"R"を代入する
+			result[i] = "R"
+		case num < 99:
+			result[i] = "SR"
+		default:
+			result[i] = "XR"
+		}
+	}
+
+	fmt.Println(result)
+}
