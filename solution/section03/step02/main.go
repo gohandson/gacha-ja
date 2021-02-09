4047b63188e91a0b13130c96463f4bfc1dd99570
tenntenn <uedatakuya@gmail.com>
2021-02-10 01:41:52 +0900

    Add step02

--- solution/section03/step02/main.go
+++ solution/section03/step02/main.go
@@ -13,7 +13,26 @@ func main() {
 	// 現在時刻をUNIX時間にしたものを種とする
 	rand.Seed(time.Now().Unix())
 
-	draw()
+	// TODO: 関数inputNを呼び出しその結果を変数nに代入
+	n := inputN()
+
+	for i := 1; i <= n; i++ {
+		draw()
+	}
+}
+
+func inputN() int {
+	var n int
+	for {
+		fmt.Print("ガチャを引く回数>")
+		fmt.Scanln(&n)
+		if n > 0 {
+			break
+		}
+		fmt.Println("もう一度入力してください")
+	}
+	// TODO: nを返す
+	return n
 }
 
 func draw() {
