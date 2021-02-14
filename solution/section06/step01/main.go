f4c0a6f9fbc454aaf7e6f9ec16471b0a88e922e5
tenntenn <uedatakuya@gmail.com>
2021-02-15 02:32:23 +0900

    Add step01

---
+++ solution/section06/step01/main.go
@@ -0,0 +1,123 @@
+// STEP05: 初期ガチャチケットの枚数をフラグで渡そう
+
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/gohandson/gacha-ja/gacha"
+)
+
+var (
+	flagCoin int
+)
+
+func init() {
+	flag.IntVar(&flagCoin, "coin", 0, "コインの初期枚数")
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	flag.Parse()
+
+	tickets, err := initialTickets()
+	if err != nil {
+		return err
+	}
+	p := gacha.NewPlayer(tickets, flagCoin)
+
+	n := inputN(p)
+	results, summary, err := gacha.DrawN(p, n)
+	if err != nil {
+		return err
+	}
+
+	if err := saveResults(results); err != nil {
+		return err
+	}
+
+	if err := saveSummary(summary); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initialTickets() (int, error) {
+	if flag.NArg() == 0 {
+		return 0, errors.New("ガチャチケットの枚数を入力してください")
+	}
+
+	num, err := strconv.Atoi(flag.Arg(0))
+	if err != nil {
+		return 0, err
+	}
+
+	return num, nil
+}
+
+func inputN(p *gacha.Player) int {
+
+	max := p.DrawableNum()
+	fmt.Printf("ガチャを引く回数を入力してください（最大:%d回）\n", max)
+
+	var n int
+	for {
+		fmt.Print("ガチャを引く回数>")
+		fmt.Scanln(&n)
+		if n > 0 && n <= max {
+			break
+		}
+		fmt.Printf("1以上%d以下の数を入力してください\n", max)
+	}
+
+	return n
+}
+
+func saveResults(results []*gacha.Card) (rerr error) {
+	f, err := os.Create("results.txt")
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil && rerr != nil {
+			rerr = err
+		}
+	}()
+
+	for _, result := range results {
+		fmt.Fprintln(f, result)
+	}
+
+	return nil
+}
+
+func saveSummary(summary map[gacha.Rarity]int) (rerr error) {
+	f, err := os.Create("summary.txt")
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil && rerr != nil {
+			rerr = err
+		}
+	}()
+
+	for rarity, count := range summary {
+		fmt.Fprintf(f, "%s %d\n", rarity.String(), count)
+	}
+
+	return nil
+}
