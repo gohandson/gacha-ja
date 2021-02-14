c8877416e7aef471e05ac7fc7217b84d62f486e5
tenntenn <uedatakuya@gmail.com>
2021-02-15 04:30:03 +0900

    Add step02

---
+++ solution/section07/step02/gacha/player.go
@@ -0,0 +1,36 @@
+package gacha
+
+import "errors"
+
+type Player struct {
+	tickets int // ガチャ券の枚数
+	coin    int // コイン
+}
+
+func NewPlayer(tickets, coin int) *Player {
+	return &Player{tickets: tickets, coin: coin}
+}
+
+// プレイヤーが行えるガチャの回数
+func (p *Player) DrawableNum() int {
+	// ガチャ券は1枚で1回、コインは10枚で1回ガチャが行える
+	return p.tickets + p.coin/10
+}
+
+func (p *Player) draw(n int) error {
+
+	if p.DrawableNum() < n {
+		return errors.New("ガチャ券またはコインが不足しています")
+	}
+
+	// ガチャ券から優先的に使う
+	if p.tickets > n {
+		p.tickets -= n
+		return nil
+	}
+
+	p.tickets = 0
+	p.coin -= n * 10 // 1回あたり10枚消費する
+
+	return nil
+}
---
+++ solution/section07/step02/main.go
@@ -0,0 +1,55 @@
+// STEP01: ガチャAPIを使ってみよう
+
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gohandson/gacha-ja/gacha"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	p := gacha.NewPlayer(10, 100)
+	// ※本当はハンドラ間で競合が起きるのでNG
+	play := gacha.NewPlay(p)
+
+	http.HandleFunc("/draw", func(w http.ResponseWriter, r *http.Request) {
+		if play.Draw() {
+			fmt.Fprintln(w, play.Result())
+		}
+
+		if err := play.Err(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintln(w, "残り:", p.DrawableNum())
+	})
+
+	return http.ListenAndServe(":8080", nil)
+}
+
+func initialTickets() (int, error) {
+	if flag.NArg() == 0 {
+		return 0, errors.New("ガチャチケットの枚数を入力してください")
+	}
+
+	num, err := strconv.Atoi(flag.Arg(0))
+	if err != nil {
+		return 0, fmt.Errorf("ガチャチケット数のパース(%q):%w", flag.Arg(0), err)
+	}
+
+	return num, nil
+}
