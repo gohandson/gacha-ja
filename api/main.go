cd921b49010220c24b95d46a59b051acd4de3d83
tenntenn <uedatakuya@gmail.com>
2021-02-19 01:57:02 +0900

    Add delay

--- api/main.go
+++ api/main.go
@@ -16,6 +16,9 @@ func main() {
 	rand.Seed(time.Now().Unix())
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.FormValue("delay") != "" {
+			time.Sleep(delay())
+		}
 		q := r.FormValue("q")
 		items := strings.Split(q, ",")
 		dist := make(map[string]int, len(items))
@@ -61,4 +64,36 @@ func main() {
 	}
 	addr := net.JoinHostPort("", port)
 	http.ListenAndServe(addr, nil)
+}
+
+func random(dist []int) int {
+	if len(dist) == 0 {
+		return -1
+	}
+
+	var sum int
+	for _, d := range dist {
+		sum += d
+	}
+
+	r := rand.Intn(sum)
+	for i, d := range dist {
+		r -= d
+		if r <= 0 {
+			return i
+		}
+	}
+
+	return dist[len(dist)-1]
+}
+
+func delay() time.Duration {
+	return []time.Duration{
+		0,
+		100 * time.Millisecond,
+		1 * time.Second,
+		10 * time.Second,
+		30 * time.Second,
+		60 * time.Second,
+	}[random([]int{20, 20, 20, 20, 20})]
 }
