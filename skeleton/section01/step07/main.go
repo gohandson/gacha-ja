ba7d29639fda3c66a5ebf1e389d21e678de13bfa
Shinnopo <sin0225skyhigh@icloud.com>
2021-12-07 22:13:43 +0900

    for文を追加

--- skeleton/section01/step07/main.go
+++ skeleton/section01/step07/main.go
@@ -15,6 +15,7 @@ func main() {
 	rand.Seed(time.Now().Unix())
 
 	// TODO: 変数iを1から11まで増やしていき処理を10回繰り返す
+	for i := 1; i <= 11; i += 1 {
 
 		// 0から99までの間で乱数を生成する
 		num := rand.Intn(100)
@@ -34,4 +35,5 @@ func main() {
 		default:
 			fmt.Println("XR")
 		}
+	}
 }
