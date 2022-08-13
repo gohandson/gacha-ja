847344926e3f7174e2d6aca8eedda9d9a9f543e2
taiki yamashita <taikity0808@gmail.com>
2022-08-13 20:34:11 +0900

    feat: step01

--- skeleton/section02/step01/main.go
+++ skeleton/section02/step01/main.go
@@ -15,8 +15,10 @@ func main() {
 	rand.Seed(time.Now().Unix())
 
 	// TODO: 長さ11の文字列型の配列を変数resultとして定義する
+	var result [11]string
 
-	for i := 0; /* TODO: 継続条件をiがresultの要素数より小さい場合にする */; i++ {
+	/* TODO: 継続条件をiがresultの要素数より小さい場合にする */
+	for i := 0; i < len(result); i++ {
 
 		// 0から99までの間で乱数を生成する
 		num := rand.Intn(100)
@@ -29,6 +31,7 @@ func main() {
 			result[i] = "ノーマル"
 		case num < 95:
 			// TODO: resultのi番目に"R"を代入する
+			result[i] = "R"
 		case num < 99:
 			result[i] = "SR"
 		default:
