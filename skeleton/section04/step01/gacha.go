75f3bc7ec0adc8ffb0331a2321efa0b359f631f2
taiki yamashita <taikity0808@gmail.com>
2022-08-17 10:12:36 +0900

    feat: step01

--- skeleton/section04/step01/gacha.go
+++ skeleton/section04/step01/gacha.go
@@ -1,3 +1,35 @@
 package main
 
+import "math/rand"
+
 // TODO: drawN関数やdraw関数をここに移す
+func drawN(p *player, n int) ([]*card, map[rarity]int) {
+	p.draw(n)
+
+	results := make([]*card, n)
+	summary := make(map[rarity]int)
+	for i := 0; i < n; i++ {
+		results[i] = draw()
+		summary[results[i].rarity]++
+	}
+
+	// 変数resultsとsummaryの値を戻り値として返す
+	return results, summary
+}
+
+func draw() *card {
+	num := rand.Intn(100)
+
+	switch {
+	case num < 80:
+		return &card{rarity: rarityN, name: "スライム"}
+	case num < 95:
+		return &card{rarity: rarityR, name: "オーク"}
+	case num < 99:
+		// TODO: rarityフィールドがraritySRで
+		// nameフィールドが"ドラゴン"であるcard構造体のポインタを返す
+		return &card{rarity: raritySR, name: "ドラゴン"}
+	default:
+		return &card{rarity: rarityXR, name: "イフリート"}
+	}
+}
